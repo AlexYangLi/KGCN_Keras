1331463701e1ebee6b18b31b3d1560650fdd2fc0
alex_yang <2041462766@qq.com>
2019-09-25 10:14:23 +0800

    refactor: update kgcn.py

--- models/kgcn.py
+++ models/kgcn.py
@@ -147,5 +147,4 @@ class KGCN(BaseModel):
         acc = accuracy_score(y_true=y_true, y_pred=y_pred)
         f1 = f1_score(y_true=y_true, y_pred=y_pred)
 
-        print(f'Logging Info - auc: {auc}, acc: {acc}, f1: {f1}')
         return auc, acc, f1
